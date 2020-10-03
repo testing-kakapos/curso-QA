0a4636f98f76af536b09d6acddba36ccde462125
iambouali <tarek@correo.ugr.es>
2020-10-03 19:44:28 +0200

    add map files

--- flaskr/app/map.py
+++ flaskr/app/map.py
@@ -6,3 +6,13 @@ import dash_html_components as html
 import plotly.express as px
 import pandas as pd
 
+token = open(".mapbox_token").read() # you will need your own token
+
+us_cities = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/us-cities-top-1k.csv")
+
+
+fig = px.scatter_mapbox(us_cities, lat="lat", lon="lon", hover_name="City", hover_data=["State", "Population"],
+                        color_discrete_sequence=["fuchsia"], zoom=3, height=300)
+fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+fig.show()
